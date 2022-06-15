d95c24b60d65c692ec1a65ac23648ec5434c8748
LIZXP <Peter.xli@outlook.com>
2022-06-15 13:21:57 -0600

    test Move-up command to server

--- client.js
+++ client.js
@@ -13,7 +13,12 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("you are connected to the server.");
     conn.write("Name: LZX");
+    conn.write("Move: up");
   });
+
+  // conn.on("connect", () => {
+  //   conn.write("Move: up");
+  // });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data.trim());
